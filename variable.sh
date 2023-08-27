7a1879d990fb18215a9c80499e2d2ab81eeb0710
manviswa <viswamandem@gmail.com>
2023-08-27 17:14:51 +0530

    variable script

--- variable.sh
+++ variable.sh
@@ -3,6 +3,6 @@
 #shell script run the command inside $() and store the output into variable called $DATE.
 
 DATE=$(date)
-COMMAND=$($?)
+COMMAND=$(echo $?)
 echo "This is scrtip was executed at timestamp: $DATE"
 echo "shows command status: $COMMAND"
