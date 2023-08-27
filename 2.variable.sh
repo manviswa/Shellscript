f36f39a71c15a224a0a4c494920bf10b6880bf93
manviswa <viswamandem@gmail.com>
2023-08-27 17:19:52 +0530

    variable script

---
+++ 2.variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello World"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Hello $PERSON1, Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: Im fine $PERSON1, How are you?"
