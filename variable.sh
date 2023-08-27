33c80f7bc0ad214ba118259de38056d074e2ed16
manviswa <viswamandem@gmail.com>
2023-08-27 17:16:30 +0530

    variable script

--- variable.sh
+++ variable.sh
@@ -4,5 +4,8 @@
 
 DATE=$(date)
 COMMAND=$(echo $?)
+WHOAMI=$(whoami)
 echo "This is scrtip was executed at timestamp: $DATE"
 echo "shows command status: $COMMAND"
+echo " this script was executed by: $WHOAMI"
+
