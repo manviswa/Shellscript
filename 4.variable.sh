823a5d73574cf57975fd3123ba1559b213ec957c
manviswa <viswamandem@gmail.com>
2023-08-27 17:46:56 +0530

    variable script

--- 4.variable.sh
+++ 4.variable.sh
@@ -5,3 +5,9 @@ echo "Enter the username from command line"
 read USERNAME # this is the variable where the value of the one user entered 
 
 echo "user name is $USERNAME"
+
+echo " enter the password"
+
+read PASSWORD
+
+echo " Password is $PASSWORD"
